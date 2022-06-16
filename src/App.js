345143aa31a2fbda4d205b19be2a98fb53669634
unknown <bibijaveriya376@gmail.com>
2022-06-16 09:07:13 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import SignUp from './signup/SignUpPage';
 import Login from './Login/LoginPage';
 import BookFlight from './BookFlight/bookFlight';
+import PageNotFound from './404/pagenotfound';
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
     <Routes>
-    <Route path='/' element={<SignUp/>}/>
-    <Route path='/login' element={<Login/>}/>
+     <Route path='/' element={<SignUp/>}/>
+     <Route path='/login' element={<Login/>}/>
     <Route path='/book' element={<BookFlight/>}/>
+    <Route path='*' element={<PageNotFound />} />
     
       </Routes>
     </BrowserRouter>
